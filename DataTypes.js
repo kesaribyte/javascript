5eb6c3a88be8e779e924df2bc496ad60d2e7ed45
kesaribyte <shitoleomraje12@gmail.com>
2026-08-05 18:38:54 +0530

    add files

---
+++ DataTypes.js
@@ -0,0 +1,12 @@
+/* data types */
+
+//  console.log(typeof "")   he aaplya la sang t ki kon t data type ahe  o/p = string
+//  console.log(typeof null) = object null ch type That's a error or languge
+// 1.Null type => The Null type is inhabited by exactly one value :null.
+// 2.Undefine type => it's means value is not assign yet
+// 3.Boolen => True or false
+// 4.String => ""   e.g("omraje")
+// 5.Symbol => unique sathi use karty t..
+// 6.number =>  21023 × (2 - 2-52)
+// 7.Bigint => above number of above
+// 8.Object => 
