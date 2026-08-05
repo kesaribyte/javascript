3dd625ba01af80ecbf9bbfdf3cde052f7ea91cff
kesaribyte <shitoleomraje12@gmail.com>
2026-08-05 18:42:54 +0530

    add files

---
+++ variables.js
@@ -0,0 +1,18 @@
+const accountId = 12354    
+let accountEmail = "omraje@gmail.ronaldo"
+var accountPassword = "2345"
+accountCity = "pune"
+var accountStates;
+//  when we not give value then the out put is undefined
+
+    //  accountId = 2 not allow
+
+    accountEmail = "ganesh@23.com"
+    accountPassword ="2398"
+    accountCity = "mumbai"
+
+// console.log(accountId)
+
+//  below table is use for a so many thing to do that's why we use 
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountStates])
