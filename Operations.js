645b0b4718c3763a9a11bfb7401d0b5473adafd3
kesaribyte <shitoleomraje12@gmail.com>
2026-08-12 01:34:30 +0530

    add notes

---
+++ Operations.js
@@ -0,0 +1,39 @@
+
+ let value = 3
+ let negvalue = -value
+
+//  console.log(negvalue)
+
+// *** Operations*** //
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)  it's means 2 power of 3
+// console.log(3%4)
+// console.log(3/2)
+
+let str1 = "hello"
+let str2 = " Omraje"
+
+let str3 = str1 + str2
+// console.log(str3)
+
+//  in java script that's simple calcalation are works well but we seen in blelow when we add sring number in there then the 
+// problem is start they give you like 
+
+// console.log("1" + 2)
+// that's answer is give to as is 12 not a 3 
+
+//console.log("1"+ 3 + 4)
+// answer == 134
+
+//console.log(1 + 3 + "4")
+// that's answer is == 44  first ans second calculate but not 3rd
+
+
+//     ------------- why the string conversion in number is confusion? --------------------------//
+   
+//------> String-to-number conversion is confusing because computers view strings and numbers through entirely different data architectures, leading to unexpected behavior during automatic type coercion, parsing, and formatting.
+
+console.log(+"")
