e3acb93696e63eda55ed36085f400cc4ce21d369
kesaribyte <shitoleomraje12@gmail.com>
2026-08-12 17:59:30 +0530

    add compersion file

---
+++ Compersion.js
@@ -0,0 +1,19 @@
+
+
+
+
+// console.log(2 < 1)  //--------> false
+// console.log(3 > 2)  //--------> true     the optput is in boolen 
+// console.log (2 <=3 ) //-------> true
+
+
+// console.log( null > 0 ); // false
+// console.log(null == 0 ); // false 
+// console.log(null >= 0);  // true
+            
+           // the reanson in that equallity check == and compersions >< >= >= woeks diffrently     2>. Comparisons conver null ti a number, trating it as 0.    that's why (3) null >= 0 is ture ans (1) null> 0 is false
+
+//--------- strict check -----------//
+        //    ===
+
+        // console.log("2" === 2) they check a datatype first
