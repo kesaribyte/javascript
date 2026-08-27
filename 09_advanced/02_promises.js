18f4a609abc26b00869f6572541f9fd88814b1c2
kesaribyte <shitoleomraje12@gmail.com>
2026-08-28 00:09:20 +0530

    missing then

--- 09_advanced/02_promises.js
+++ 09_advanced/02_promises.js
@@ -93,10 +93,3 @@ fetch('https://api.github.com/users/hiteshchoudhary')
 .then((response) => {
     return response.json()
 })
-.then((data) => {
-    console.log(data);
-})
-.catch((error) => console.log(error))
-
-// promise.all
-// yes this is also available, kuch reading aap b kro.
